3f9f035eb30a028a95805388c9e80a6875640d2e
Louis Shore <louis.shore@ynov.com>
2025-03-20 19:16:01 +0100

    class(Player): created

---
+++ src/main/java/fr/ynov/uno/game/player/Player.java
@@ -0,0 +1,17 @@
+package fr.ynov.uno.game.player;
+
+import fr.ynov.uno.game.Game;
+import fr.ynov.uno.game.card.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private List<Card> playerCards;
+
+    public void playCard(){
+    }
+    public void pickupCard(){
+    }
+
+}
