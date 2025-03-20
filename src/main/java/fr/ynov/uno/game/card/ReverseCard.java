6264a4ceb95940c1ef02fda5dfad0d5140eabdb5
Louis Shore <louis.shore@ynov.com>
2025-03-20 19:17:10 +0100

    class(ReverseCard): created

---
+++ src/main/java/fr/ynov/uno/game/card/ReverseCard.java
@@ -0,0 +1,15 @@
+package fr.ynov.uno.game.card;
+
+import fr.ynov.uno.game.Game;
+
+import java.awt.*;
+
+public class ReverseCard extends PowerCard{
+    ReverseCard(String type, Color color, int number) {
+        super(type, color, number);
+    }
+
+    @Override
+    public void usePower() {
+    }
+}
