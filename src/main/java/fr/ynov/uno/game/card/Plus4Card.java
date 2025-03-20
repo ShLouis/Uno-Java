facee96c3b12207907f094412f2fd2d5e221ade3
Louis Shore <louis.shore@ynov.com>
2025-03-20 19:16:46 +0100

    class(Plus4Card): created

---
+++ src/main/java/fr/ynov/uno/game/card/Plus4Card.java
@@ -0,0 +1,14 @@
+package fr.ynov.uno.game.card;
+
+import java.awt.*;
+
+public class Plus4Card extends PowerCard{
+
+    Plus4Card(String type, Color color, int number) {
+        super(type, color, number);
+    }
+
+    @Override
+    public void usePower() {
+    }
+}
