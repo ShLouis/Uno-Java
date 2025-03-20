f8cfabbc4a3857fcd984ac18ea556ec09a1f2dfe
Louis Shore <louis.shore@ynov.com>
2025-03-20 19:17:33 +0100

    class(SkipCard): created

---
+++ src/main/java/fr/ynov/uno/game/card/SkipCard.java
@@ -0,0 +1,14 @@
+package fr.ynov.uno.game.card;
+
+import java.awt.*;
+
+public class SkipCard extends PowerCard{
+    SkipCard(String type, Color color, int number) {
+        super(type, color, number);
+    }
+
+    @Override
+    public void usePower() {
+
+    }
+}
