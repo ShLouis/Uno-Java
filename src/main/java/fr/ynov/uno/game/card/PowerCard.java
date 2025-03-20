bf737791dea33fd2741efb9515d0ee0de6850a2e
Louis Shore <louis.shore@ynov.com>
2025-03-20 15:38:32 +0100

    class(PowerCard): created

--- src/main/java/fr/ynov/uno/game/card/PowerCard.java
+++ src/main/java/fr/ynov/uno/game/card/PowerCard.java
@@ -1,4 +1,14 @@
 package fr.ynov.uno.game.card;
 
-public class PowerCard {
+import java.awt.*;
+import java.util.Objects;
+
+public abstract class PowerCard extends Card {
+
+    PowerCard(String type, Color color, int number) {
+        super(type, color, number);
+    }
+
+    public abstract boolean usePower();
+
 }
