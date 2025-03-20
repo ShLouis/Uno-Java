9d6dfa191ead7793a7d08309f6da80dd1532d5fa
Louis Shore <louis.shore@ynov.com>
2025-03-20 19:18:10 +0100

    class(Plus2Card): created

---
+++ src/main/java/fr/ynov/uno/game/card/Plus2Card.java
@@ -0,0 +1,15 @@
+package fr.ynov.uno.game.card;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class Plus2Card extends PowerCard{
+    Plus2Card(String type, Color color, int number) {
+        super(type, color, number);
+    }
+
+    @Override
+    public  void usePower() {
+
+    }
+}
