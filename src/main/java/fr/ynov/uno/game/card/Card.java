fa0466df3c2585a88269ba2233be880a3dcb1d9c
Louis Shore <louis.shore@ynov.com>
2025-03-20 21:42:38 +0100

    class(Card): Updated

--- src/main/java/fr/ynov/uno/game/card/Card.java
+++ src/main/java/fr/ynov/uno/game/card/Card.java
@@ -1,17 +1,36 @@
 package fr.ynov.uno.game.card;
 
+import fr.ynov.uno.game.player.Player;
+
 import java.awt.*;
+import java.util.Objects;
 
 public abstract class Card {
     protected Color color;
     protected Integer number;
     protected String type;
 
-    Card(String type, Color color, int number) {
+    Card(String type, Color color, Integer number) {
         this.type = type;
         this.color = color;
         this.number = number;
     }
 
-    public abstract boolean canBePlacedOn(Card bottomCard);
+    public Color getColor() {
+        return color;
+    }
+    public Integer getNumber() {
+        return number;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public boolean canBePlacedOn(Card bottomCard) {
+        return (this.color == null && this.number == null) || (this.color == bottomCard.color) || (Objects.equals(this.number, bottomCard.number));
+    }
+
+    public void show(){
+        System.out.println(type);
+    }
 }
