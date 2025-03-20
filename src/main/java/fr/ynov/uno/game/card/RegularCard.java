a1b3fc69abecf7141765b7042c36d5282468267a
Louis Shore <louis.shore@ynov.com>
2025-03-20 21:47:05 +0100

    class(RegularCard): Updated

--- src/main/java/fr/ynov/uno/game/card/RegularCard.java
+++ src/main/java/fr/ynov/uno/game/card/RegularCard.java
@@ -3,22 +3,9 @@ package fr.ynov.uno.game.card;
 import java.awt.*;
 import java.util.Objects;
 
-public class RegularCard extends Card{
+public class RegularCard extends Card {
 
-    public RegularCard(String type, Color color,int number){
-        super(type,color,number);
+    public RegularCard(String type, Color color, Integer number) {
+        super(type, color, number);
     }
-
-    public String getType(){
-        return type;
-    }
-    public Color getColor(){
-        return color;
-    }
-
-    @Override
-    public boolean canBePlacedOn(Card bottomCard) {
-        return (this.color == null && this.number == null) || (this.color == bottomCard.color) || (Objects.equals(this.number, bottomCard.number));
-    }
-
 }
