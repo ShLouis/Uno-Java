11fb9e4e1c88069837a1415b0491ae1f7a4c38e6
Louis Shore <louis.shore@ynov.com>
2025-03-24 16:20:20 +0100

    class(plus2Card): usePower method

--- src/main/java/fr/ynov/uno/game/card/Plus2Card.java
+++ src/main/java/fr/ynov/uno/game/card/Plus2Card.java
@@ -1,15 +1,26 @@
 package fr.ynov.uno.game.card;
 
+import fr.ynov.uno.game.Game;
+
 import java.awt.*;
-import java.util.Objects;
 
 public class Plus2Card extends PowerCard{
-    public Plus2Card(String type, Color color, Integer number) {
-        super(type, color, number);
+    public Plus2Card(String name,String type, Color color, Integer number) {
+        super(name,type, color, number);
     }
 
     @Override
-    public  void usePower() {
+    public void usePower(Game game) {
+        System.out.println("player" +game.getCurrentPlayer()+"used a plus 2 card");
+        int next=1;
+        if (game.getCurrentPlayer()==game.getPlayers().size()-1){
+            next=-game.getPlayers().size()-1;
+        }
+        for (int i=0;i<2;i++) {
+            game.getPlayers().get(game.getCurrentPlayer()+next).getPlayerCards().add(game.takeCard());
+        }
 
     }
+
+
 }
