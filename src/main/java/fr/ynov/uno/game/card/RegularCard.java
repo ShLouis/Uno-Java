d8730ddd1608c9048b9a456c230eacc9d06e986b
Louis Shore <louis.shore@ynov.com>
2025-03-24 16:22:39 +0100

    class(RegularCard): updated

--- src/main/java/fr/ynov/uno/game/card/RegularCard.java
+++ src/main/java/fr/ynov/uno/game/card/RegularCard.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 
 public class RegularCard extends Card {
 
-    public RegularCard(String type, Color color, Integer number) {
-        super(type, color, number);
+    public RegularCard(String name,String type, Color color, Integer number) {
+        super(name,type, color, number);
     }
 }
