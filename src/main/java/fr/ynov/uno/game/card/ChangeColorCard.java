14a2d567f3b77ada370ac98426d92ef875aa0a8b
Louis Shore <louis.shore@ynov.com>
2025-03-24 16:19:39 +0100

    class(ChangeColorCard): usePower method

--- src/main/java/fr/ynov/uno/game/card/ChangeColorCard.java
+++ src/main/java/fr/ynov/uno/game/card/ChangeColorCard.java
@@ -1,15 +1,34 @@
 package fr.ynov.uno.game.card;
 
+import fr.ynov.uno.game.Game;
+import fr.ynov.uno.game.player.Player;
+
 import java.awt.*;
+import java.util.Scanner;
 
 public class ChangeColorCard extends PowerCard{
 
-    public ChangeColorCard(String type, Color color, Integer number) {
-        super(type, color, number);
+    public ChangeColorCard(String name,String type, Color color, Integer number) {
+        super(name,type, color, number);
     }
 
     @Override
-    public void usePower() {
-        this.color=Color.RED;
+    public void usePower(Game game) {
+        System.out.println("player" +game.getCurrentPlayer()+"used a change color card");
+        if (game.getCurrentPlayer()==0) {
+            System.out.println("Pick a color");
+            System.out.println("1: RED\n2: BLUE\n3: GREEN\n4: YELLOW");
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+            this.color = game.getColors().get(choice - 1);
+            System.out.println("the color has changed to " + game.getColors().get(choice - 1));
+        }else{
+            for(Card c:game.getPlayers().get(game.getCurrentPlayer()).getPlayerCards()){
+                if(c.getColor() != null){
+                    this.color = c.getColor();
+                }
+            }
+            System.out.println("the color has changed to " + this.color);
+        }
     }
 }
