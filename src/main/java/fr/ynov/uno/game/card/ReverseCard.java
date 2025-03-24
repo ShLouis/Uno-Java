867dd8c6780dd3f8b37b9086f94fd4dff43f3bd9
Louis Shore <louis.shore@ynov.com>
2025-03-24 16:21:01 +0100

    class(ReverseCard): usePower method

--- src/main/java/fr/ynov/uno/game/card/ReverseCard.java
+++ src/main/java/fr/ynov/uno/game/card/ReverseCard.java
@@ -5,11 +5,13 @@ import fr.ynov.uno.game.Game;
 import java.awt.*;
 
 public class ReverseCard extends PowerCard{
-    public ReverseCard(String type, Color color, Integer number) {
-        super(type, color, number);
+    public ReverseCard(String name,String type, Color color, Integer number) {
+        super(name,type, color, number);
     }
 
     @Override
-    public void usePower() {
+    public void usePower(Game game) {
+        System.out.println("player" +game.getCurrentPlayer()+"used a reverse card");
+        game.setDirection(-game.getDirection());
     }
 }
