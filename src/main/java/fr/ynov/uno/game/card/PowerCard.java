7d31bbd2a075a7d0c4a6e3d63160e0332194c6e1
Louis Shore <louis.shore@ynov.com>
2025-03-24 16:22:20 +0100

    class(PowerCard): updated

--- src/main/java/fr/ynov/uno/game/card/PowerCard.java
+++ src/main/java/fr/ynov/uno/game/card/PowerCard.java
@@ -1,13 +1,15 @@
 package fr.ynov.uno.game.card;
 
+import fr.ynov.uno.game.Game;
+
 import java.awt.*;
 import java.util.Objects;
 
 public abstract class PowerCard extends Card {
 
-    PowerCard(String type, Color color, Integer number) {
-        super(type, color, number);
+    PowerCard(String name, String type, Color color, Integer number) {
+        super(name, type, color, number);
     }
 
-    public abstract void usePower();
+    public abstract void usePower(Game game);
 }
